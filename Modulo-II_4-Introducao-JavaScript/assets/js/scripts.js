4ce84c800558234cce6ea0dd505ccb983747fa41
tfavrin <talita_favrin@hotmail.com>
2022-07-23 23:40:37 -0300

    Projeto entregue

---
+++ Modulo-II_4-Introducao-JavaScript/assets/js/scripts.js
@@ -0,0 +1,17 @@
+var currentNumberWrapper = document.getElementById('currentNumber');
+var currentNumber = 0;
+
+
+function increment () {
+  if (currentNumber < 10) {
+    currentNumber = currentNumber + 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
+}
+
+function decrement () {
+  if (currentNumber > 0) {
+    currentNumber = currentNumber - 1;
+    currentNumberWrapper.innerHTML = currentNumber;
+  }
+}
